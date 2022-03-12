a1db364f808b1d39162c7864c782060f7ff705e5
Nguyenvanvu123 <101451269+Nguyenvanvu123@users.noreply.github.com>
2022-03-12 15:47:04 +0700

    Add files via upload

---
+++ baitap1.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+main()
+{
+    cout<<"Ten: "<<"Nguyen Van Vu"<<endl;
+    cout<<"MSSV: "<<"106210110"<<endl;
+    cout<<"Lop: "<<"21DTCLC1"<<endl;
+    return 0;
+}
